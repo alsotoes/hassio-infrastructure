84ee0a89dd03199d8cec54629b39b0a4116fc22e
alsotoes <alsotoes@gmail.com>
2024-08-23 14:08:41 -0600

    fix shellcheck errors and default values on run.sh

--- etcd/run.sh
+++ etcd/run.sh
@@ -8,14 +8,14 @@ set -ex
 ( set -o pipefail 2> /dev/null ) || true
 
 /usr/local/bin/etcd \
-  --name ${NODE_NAME} \
+  --name "${NODE_NAME:-etcd}" \
   --data-dir /var/lib/etcd \
   --listen-client-urls http://0.0.0.0:2379 \
   --advertise-client-urls http://0.0.0.0:2379 \
   --listen-peer-urls http://0.0.0.0:2380 \
   --initial-advertise-peer-urls http://0.0.0.0:2380 \
-  --initial-cluster ${NODE_NAME}=http://0.0.0.0:2380 \
-  --initial-cluster-state ${INITIAL_CLUSTER_STATE} \
-  --log-level ${LOG_LEVEL} \
-  --logger ${LOGGER} \
+  --initial-cluster "${NODE_NAME:-etcd}"=http://0.0.0.0:2380 \
+  --initial-cluster-state "${INITIAL_CLUSTER_STATE:-new}" \
+  --log-level "${LOG_LEVEL:-info}" \
+  --logger "${LOGGER:-capnslog}" \
   --log-outputs stderr
