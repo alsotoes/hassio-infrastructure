98ed0b3a9df46b5c3af8df0fcd07c0fccf0d1b4f
alsotoes <alsotoes@gmail.com>
2024-08-23 23:07:54 -0600

    wrong [ if file exists ]

--- etcd/rootfs/entrypoint.sh
+++ etcd/rootfs/entrypoint.sh
@@ -11,7 +11,7 @@ ARCH="$(echo $ARCH)"
 DOWNLOAD_URL="$(echo $DOWNLOAD_URL)"
 ETCD__VER="$(echo $ETCD__VER)"
 
-if [ -f "/usr/local/bin/etcd" ]
+if [ -x "/usr/local/bin/etcd" ]
 then
     # already installed, so do nothing 
     if [ "${ARCH}" = "aarch64" ]
