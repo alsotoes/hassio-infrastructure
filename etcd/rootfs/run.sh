f53da8e9fb8f7b298e5131f41a5c9eb161bbbda7
alsotoes <alsotoes@gmail.com>
2024-08-28 17:23:04 -0600

    after breaking the addon =/

--- etcd/rootfs/run.sh
+++ etcd/rootfs/run.sh
@@ -1,21 +1,32 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bashio
 # -*- coding: utf-8 -*-
 
-# Exit on error
-set -ex
+declare etcd_name
+declare etcd_initial_cluster_state
+declare etcd_initial_cluster
+declare etcd_log_level
 
-# Exit on pipe fails (if possible)
-( set -o pipefail 2> /dev/null ) || true
+etcd_name="$(bashio::config 'node_name')"
+etcd_initial_cluster_state="$(bashio::config 'initial_cluster_state')"
+etcd_initial_cluster="$(bashio::config 'node_name')=http://0.0.0.0:2380"
+etcd_log_level="$(bashio::config 'log_level')"
 
-ETCD_NAME="$(bashio::config 'node_name')" \
-ETCD_INITIAL_CLUSTER_STATE="$(bashio::config 'initial_cluster_state')" \
-ETCD_INITIAL_CLUSTER="${ETCD_NAME}=http://0.0.0.0:2380" \
-ETCD_LOG_LEVEL="$(bashio::config 'log_level')" \
+if [ bashio::config.has_value 'node_name' ] ; then
+    export ETCD_NAME=${etcd_name}
+    export ETCD_INITIAL_CLUSTER_STATE=${etcd_initial_cluster_state}
+    export ETCD_INITIAL_CLUSTER="${etcd_initial_cluster}"
+    export ETCD_LOG_LEVEL="${etcd_log_level}"
+else
+    export ETCD_NAME="etcd"
+    export ETCD_INITIAL_CLUSTER_STATE="new"
+    export ETCD_INITIAL_CLUSTER="etcd=http://0.0.0.0:2380"
+    export ETCD_LOG_LEVEL="info"
+fi
 
 /usr/local/bin/etcd \
-  --data-dir /data \
-  --listen-client-urls http://0.0.0.0:2379 \
-  --advertise-client-urls http://0.0.0.0:2379 \
-  --listen-peer-urls http://0.0.0.0:2380 \
-  --initial-advertise-peer-urls http://0.0.0.0:2380 \
-  --log-outputs stderr
+    --data-dir /data \
+    --listen-client-urls http://0.0.0.0:2379 \
+    --advertise-client-urls http://0.0.0.0:2379 \
+    --listen-peer-urls http://0.0.0.0:2380 \
+    --initial-advertise-peer-urls http://0.0.0.0:2380 \
+    --log-outputs stderr
