ed2032ea0d141db9b9870f19c5b2d1fb8666e06b
alsotoes <alsotoes@gmail.com>
2024-08-28 14:42:52 -0600

    loading env vars at the same time ot etcd

--- etcd/rootfs/run.sh
+++ etcd/rootfs/run.sh
@@ -7,14 +7,15 @@ set -ex
 # Exit on pipe fails (if possible)
 ( set -o pipefail 2> /dev/null ) || true
 
+ETCD_NAME="$(bashio::config 'node_name')" \
+ETCD_INITIAL_CLUSTER_STATE="$(bashio::config 'initial_cluster_state')" \
+ETCD_INITIAL_CLUSTER="${ETCD_NAME}=http://0.0.0.0:2380" \
+ETCD_LOG_LEVEL="$(bashio::config 'log_level')" \
+
 /usr/local/bin/etcd \
-  --name "$(bashio::config 'NODE_NAME')" \
   --data-dir /data \
   --listen-client-urls http://0.0.0.0:2379 \
   --advertise-client-urls http://0.0.0.0:2379 \
   --listen-peer-urls http://0.0.0.0:2380 \
   --initial-advertise-peer-urls http://0.0.0.0:2380 \
-  --initial-cluster "$(bashio::config 'NODE_NAME')"=http://0.0.0.0:2380 \
-  --initial-cluster-state "$(bashio::config 'INITIAL_CLUSTER_STATE')" \
-  --log-level "$(bashio::config 'LOG_LEVEL')" \
   --log-outputs stderr
