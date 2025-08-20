962112df548084b069e750ffa9ad22d8bc06b29b
alsotoes <alsotoes@gmail.com>
2025-08-20 02:54:01 -0600

    fixing CAP issues on docker

---
+++ smb1-proxy/CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## 1.0.1
+
+- Fix CAP issues for error "Unable to apply new capability set"
--- smb1-proxy/run.sh
+++ smb1-proxy/run.sh
@@ -1,20 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-# Variables passed via add-on config (see config.json)
-OLD_SERVER="${OLD_SERVER:-192.168.1.50}"
-OLD_SHARE="${OLD_SHARE:-Share}"
-OLD_USER="${OLD_USER:-guest}"
-OLD_PASS="${OLD_PASS:-}"
-MOUNTPOINT="/data/oldshare"
+# Mount SMB1 share
+mkdir -p /data/oldshare
 
-mkdir -p "$MOUNTPOINT"
+mount -t cifs "//$OLD_SERVER/$OLD_SHARE" /data/oldshare \
+  -o username=$OLD_USER,password=$OLD_PASS,vers=1.0,sec=ntlm,nounix,noserverino
 
-# Mount legacy SMB1 share
-mount -t cifs "//$OLD_SERVER/$OLD_SHARE" "$MOUNTPOINT" \
-    -o username=$OLD_USER,password=$OLD_PASS,vers=1.0,sec=ntlm,nounix,noserverino
-
-# Write Samba config
+# Generate Samba config
 cat > /etc/samba/smb.conf <<EOF
 [global]
    workgroup = WORKGROUP
@@ -24,14 +17,19 @@ cat > /etc/samba/smb.conf <<EOF
    smb encrypt = required
    min protocol = SMB2
    max protocol = SMB3
+   disable netbios = yes
+   load printers = no
+   printing = bsd
+   printcap name = /dev/null
+   disable spoolss = yes
 
 [proxied]
-   path = $MOUNTPOINT
+   path = /data/oldshare
    browseable = yes
    writable = yes
    guest ok = yes
    read only = no
 EOF
 
-# Launch Samba daemons
-/usr/sbin/smbd --foreground --no-process-group
+# Run Samba without capability issues
+exec /usr/sbin/smbd --foreground --no-process-group --debug-stdout
